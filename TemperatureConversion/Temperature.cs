ccce84f8269d768f601a19f4fd22ff62006bc011
Žygimantas Matusevičius <zmatuse@corp.transunion.com>
2022-07-08 14:26:13 +0300

    clean up TemperatureConversion project

--- TemperatureConversion/Temperature.cs
+++ TemperatureConversion/Temperature.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using TemperatureConversion.TemperatureSystems;
+﻿using TemperatureConversion.TemperatureSystems;
 
 namespace TemperatureConversion
 {
