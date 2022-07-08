7c2c12fdd033bbd86f84bfc091224675b001af25
Žygimantas Matusevičius <zmatuse@corp.transunion.com>
2022-07-08 14:53:14 +0300

    Add Temperature.Parse method

--- TemperatureConversion/Temperature.cs
+++ TemperatureConversion/Temperature.cs
@@ -1,4 +1,6 @@
-﻿using TemperatureConversion.TemperatureSystems;
+﻿using System;
+using TemperatureConversion.Extensions;
+using TemperatureConversion.TemperatureSystems;
 
 namespace TemperatureConversion
 {
@@ -6,5 +8,21 @@ namespace TemperatureConversion
     {
         public TemperatureSystem TemperatureSystem { get; set; }
         public double Degrees { get; set; }
+
+        public static Temperature Parse(string temperatureExpression)
+        {
+            if (temperatureExpression == null)
+                throw new ArgumentException("Provided argument can not be null.", nameof(temperatureExpression));
+
+            var expressionParts = temperatureExpression.Split(" ");
+            if (expressionParts.Length != 2)
+                throw new ArgumentException("Provided expression is in wrong format.", nameof(temperatureExpression));
+
+            return new Temperature
+            {
+                Degrees = double.Parse(expressionParts[0]),
+                TemperatureSystem = Enum.Parse<TemperatureType>(expressionParts[1]).Create()
+            };
+        }
     }
 }
